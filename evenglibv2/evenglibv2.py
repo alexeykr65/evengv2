c79c395dee6d37a97c70e3b57f02fcfb544994ef
Alexey <alexeykr@gmail.com>
2019-12-15 00:45:04 +0300

    first commit

---
+++ evenglibv2/evenglibv2.py
@@ -0,0 +1,409 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Configure S-Terra
+#
+# alexeykr@gmail.com
+# coding=utf-8
+# import codecs
+"""
+Classes for change configuration of router on EVE-NG
+version: 2.0
+@author: alexeykr@gmail.com
+"""
+import paramiko
+import yaml
+import scp
+import re
+import time
+import netmiko as nm
+import coloredlogs
+import warnings
+import logging
+import sys
+import pexpect
+import xmltodict
+import runpy
+from subprocess import call
+#from pyats import aetest
+#from genie import testbed as tbd
+#from genie.conf import Genie
+from genie import testbed
+from netaddr import IPNetwork
+from jinja2 import Template, Environment, FileSystemLoader
+warnings.filterwarnings(action='ignore', module='.*paramiko.*')
+
+
+class MyLogging:
+    """ For logging configuration 
+    """
+
+    def __init__(self, level, log_name):
+        self.__logging_level = level
+        self.__logging_name = log_name
+
+    def get_color_logger(self):
+        logger = logging.getLogger(self.__logging_name)
+        logger.setLevel(self.__logging_level)
+        coloredlogs.install(level=self.__logging_level, logger=logger, fmt='%(name)-10s: %(funcName)-20s - %(levelname)-6s - %(message)s')
+
+        logger.propagate = False
+
+        return logger
+
+
+class EveUnlInterface:
+    """ Class for description of EVE-NG Interface """
+
+    def __init__(self, id, name, net_id):
+        self.__int_id = id
+        self.__int_name = name
+        self.__int_net_id = net_id
+        self.__int_ipv4 = ''
+        self.__int_mgmt = False
+
+    def __str__(self):
+        return f'IntID: {self.__int_id} IntName: {self.__int_name}  LinkID: {self.__int_net_id} ipv4: {self.__int_ipv4} MGMT: {self.__int_mgmt}'
+
+    @property
+    def int_id(self):
+        return self.__int_id
+
+    @property
+    def int_name(self):
+        return self.__int_name
+
+    @property
+    def int_net_id(self):
+        return self.__int_net_id
+
+    @property
+    def int_ipv4(self):
+        return self.__int_ipv4
+
+    @int_ipv4.setter
+    def int_ipv4(self, ipv4):
+        self.__int_ipv4 = ipv4
+
+    @property
+    def int_mgmt(self):
+        return self.__int_mgmt
+
+    @int_mgmt.setter
+    def int_mgmt(self, flg_mgmt):
+        self.__int_mgmt = flg_mgmt
+
+
+class EveUnl:
+    """ Class for description of EVE-NG Node """
+
+    def __init__(self, id='', name='', uuid='', nod_type='', template='', firstmac='', image='', ethernet='', nod_int=list()):
+        self.__nod_uuid = uuid
+        self.__nod_id = id
+        self.__nod_type = nod_type
+        self.__nod_name = name
+        self.__nod_template = template
+        self.__nod_firstmac = firstmac
+        self.__nod_image = image
+        self.__nod_port = ''
+        self.__nod_interfaces = nod_int
+        self.__nod_routes = list()
+
+    @property
+    def name(self):
+        return self.__nod_name
+
+    @property
+    def interfaces(self):
+        return self.__nod_interfaces
+
+    @property
+    def uuid(self):
+        return self.__nod_uuid
+
+    @property
+    def id(self):
+        return self.__nod_id
+
+    @property
+    def type(self):
+        return self.__nod_type
+
+    @property
+    def template(self):
+        return self.__nod_template
+
+    @property
+    def port(self):
+        return self.__nod_port
+
+    @port.setter
+    def port(self, nod_port):
+        self.__nod_port = nod_port
+
+    @property
+    def routes(self):
+        return self.__nod_routes
+
+    @routes.setter
+    def routes(self, nod_routes):
+        self.__nod_routes = nod_routes
+
+    @property
+    def firstmac(self):
+        return self.__nod_firstmac
+
+    def __str__(self):
+        ret = f'NodeID= {self.__nod_id}' +\
+            f'  Name = {self.__nod_name}' +\
+            f'  Port = {self.__nod_port}' +\
+            f'  Template = {self.__nod_template}' +\
+            f'  Image = {self.__nod_image}'
+        for nod_int in self.__nod_interfaces:
+            ret = ret + f'\n    {str(nod_int)}'
+        for nod_route in self.__nod_routes:
+            ret = ret + f'\n    Net Route: {str(nod_route["net_prefix"])} GW: {str(nod_route["gw"])}'
+
+        return ret
+
+
+class EveNgLab:
+    """ Get some information about lab from unl file and processes in EVE-NG """
+
+    def __init__(self, unl_file, eve_ip_host='', eve_ssh_username='root', eve_ssh_password='cisco', file_config='eveng.cfg'):
+        self.__template_list = ['csr1000vng', 'xrv', 'vios']
+        self.__unl_file = unl_file
+        self.__file_config = file_config
+        self.__eveng_conn_param = dict()
+        self.__eveng_conn_param['ip'] = eve_ip_host
+        self.__eveng_conn_param['device_type'] = "linux"
+        self.__eveng_conn_param['password'] = eve_ssh_password
+        self.__eveng_conn_param['username'] = eve_ssh_username
+        self.__local_unl_file = "lab_unl_file.unl"
+        self.__cmd_find_unl_file = f'find /opt/unetlab/ -iname "*{self.__unl_file}*.unl"'
+        self.__lab_param = dict()
+        self.__lg = MyLogging(logging.INFO, "EveNgLab")
+        self.__logger = self.__lg.get_color_logger()
+
+    def __str__(self):
+        return f'Device type : {self.__template_list}'
+
+    # @classmethod
+    def ipaddr(self, input_str, net_cfg):
+        ip_net = IPNetwork(input_str)
+        ret = ''
+        if net_cfg == 'address':
+            ret = ip_net.ip
+        elif net_cfg == 'netmask':
+            ret = ip_net.netmask
+        elif net_cfg == 'hostmask':
+            ret = ip_net.hostmask
+        elif net_cfg == 'network':
+            ret = ip_net.network
+        return ret
+
+    def eveng_conn_param(self, eve_ip_host, eve_ssh_username, eve_ssh_password):
+        self.__eveng_conn_param['ip'] = eve_ip_host
+        self.__eveng_conn_param['password'] = eve_ssh_password
+        self.__eveng_conn_param['username'] = eve_ssh_username
+
+    def __connect_to_host(self, cmd_run):
+        return_message = ""
+        # print(f'Connect to host: {self.__eveng_conn_param}')
+        try:
+            id_ssh = nm.ConnectHandler(**self.__eveng_conn_param)
+            id_ssh.read_channel()
+            find_hostname = id_ssh.find_prompt()
+            if not find_hostname:
+                time.sleep(0.1)
+                find_hostname = id_ssh.find_prompt()
+            hostname = re.match("root@([^:]*):~#", find_hostname).group(1).strip()
+            self.__logger.info(f"Connected to hostname: {hostname} with Ip : {self.__eveng_conn_param['ip']} ... OK")
+            cmd_return = id_ssh.send_command(cmd_run)
+            self.__logger.debug(f'Run command: {cmd_run} ... OK')
+            return_message += '{}\n'.format(cmd_return)
+            return return_message
+        except Exception as error:
+            self.__logger.error(f'{error} Exit script ...')
+            # logger.error("Exit script ...")
+            exit(1)
+        return return_message
+
+    def get_proc_param(self):
+        ret_msg = self.__connect_to_host('ps -ax | grep qemu_wrapper')
+        for ss in ret_msg.split("\n"):
+            proc_variable = dict()
+            self.__logger.debug(ss)
+            if re.search("uuid", ss):
+                re_res = re.match(r'[^-]*-C\s(\d*)\s-T\s\d*\s-D\s\d*\s-t\s([^-]*).*-uuid\s*([^q]*).*', ss)
+                proc_variable['uuid'] = re_res.group(3).strip('-').strip()
+                proc_variable['host_name'] = re_res.group(2).strip().lower()
+                proc_variable['port'] = re_res.group(1)
+                if proc_variable['host_name'] in self.__lab_param and proc_variable['uuid'] == self.__lab_param[proc_variable['host_name']].uuid:
+                    self.__logger.debug('uuid:{uuid} host:{host_name} telnet_port:{port}'.format_map(proc_variable))
+                    self.__lab_param[proc_variable["host_name"]].port = proc_variable['port']
+                    self.__logger.debug(f' Host: {proc_variable["host_name"]} Port: {proc_variable["port"]}')
+        return self.__lab_param
+
+    def parse_unl_file(self, name_unl_file):
+        # local_unl_file = 'eve-ng.unl'
+        self.__logger.info(f'UNL file on EVE-NG : {name_unl_file}')
+        # Find path of UNL file on EVE-NG
+        unl_param = dict()
+        with open(name_unl_file, mode='r') as id_unl:
+            content_xml = id_unl.read()
+        dict_xml = xmltodict.parse(content_xml)
+        for router in dict_xml['lab']['topology']['nodes']['node']:
+            self.__logger.debug(f'RECORD_XML: {router}')
+            if "@uuid" in router:
+                unl_nod_int = list()
+                self.__logger.debug('Name:{@name}  UUID: {@uuid}'.format_map(router))
+                # self.__logger.info(f'{router["interface"]}')
+                self.__logger.info(f'{router["@name"]} Interfaces:')
+                if 'interface' in router:
+                    nod_interface = router["interface"]
+                    if isinstance(nod_interface, list):
+                        for interf in nod_interface:
+                            self.__logger.info(f" - id: {interf['@id']}  name: {interf['@name']} net_id: {interf['@network_id']}")
+                            unl_nod_int.append(EveUnlInterface(interf['@id'], interf['@name'], interf['@network_id']))
+                    else:
+                        self.__logger.info(f"id: {nod_interface['@id']}  name: {nod_interface['@name']} net_id: {nod_interface['@network_id']}")
+                eve = EveUnl(name=router['@name'].lower(),
+                             template=router['@template'],
+                             id=router['@id'],
+                             nod_type=router['@type'],
+                             uuid=router['@uuid'],
+                             image=router['@image'],
+                             ethernet=router['@ethernet'],
+                             nod_int=unl_nod_int)
+                unl_param[router['@name'].strip().lower()] = eve
+
+        self.__logger.debug(f'UNL: {unl_param}')
+        self.__lab_param = unl_param
+        return unl_param
+
+    def get_local_unl_file(self, name_unl_file):
+        # local_unl_file = 'eve-ng.unl'
+        self.__logger.info(f'Find UNL file on EVE-NG : {name_unl_file}')
+        # Find path of UNL file on EVE-NG
+        unl_param = self.parse_unl_file(name_unl_file)
+        return unl_param
+
+    def get_remote_unl_file(self):
+        # local_unl_file = 'eve-ng.unl'
+        self.__logger.info(f'Find UNL file on EVE-NG : {self.__cmd_find_unl_file}')
+        # Find path of UNL file on EVE-NG
+        ret_msg = self.__connect_to_host(self.__cmd_find_unl_file)
+        self.__logger.debug(f"Full return: {ret_msg}")
+        path_to_unl = ret_msg.strip()
+        # print(f'Path: {path_to_unl}')
+        self.__logger.info(f"Finded unl file: {path_to_unl}")
+        # Get UNL file
+        id_conn_paramiko = paramiko.SSHClient()
+        id_conn_paramiko.set_missing_host_key_policy(paramiko.WarningPolicy)
+        id_conn_paramiko.connect(self.__eveng_conn_param['ip'], username=self.__eveng_conn_param['username'], password=self.__eveng_conn_param['password'])
+        with scp.SCPClient(id_conn_paramiko.get_transport()) as id_scp:
+            id_scp.get(path_to_unl, self.__local_unl_file)
+        self.__logger.info(f'Download file {path_to_unl} ... OK')
+        # Analyze UNL file
+        unl_param = self.parse_unl_file(self.__local_unl_file)
+        return unl_param
+
+    def load_config_yaml(self, name_config_yaml):
+        with open(name_config_yaml) as yml:
+            conf_Yaml = yaml.load(yml)
+        for router_name in conf_Yaml:
+            # print(router_name)
+            for router_int in conf_Yaml[router_name]['interfaces']:
+                for unl_int in self.__lab_param[router_name.lower()].interfaces:
+                    if unl_int.int_name == router_int['name']:
+                        # print(self.__lab_param[router_name.lower()].interfaces)
+                        unl_int.int_ipv4 = router_int['ipv4']
+                        if 'mgmt' in router_int:
+                            unl_int.int_mgmt = True
+            for router_route in conf_Yaml[router_name]['routes']:
+                nod_routes = dict()
+                nod_routes['net_prefix'] = router_route['net']
+                nod_routes['gw'] = router_route['gw']
+                self.__lab_param[router_name.lower()].routes.append(nod_routes)
+        for k in self.__lab_param:
+            for m in self.__lab_param[k].interfaces:
+                self.__logger.info(f'Node: {k} {m}')
+
+    def generate_jinja(self, tmp_name_file, out_file):
+        file_loader = FileSystemLoader('evenglibv2')
+        env = Environment(loader=file_loader)
+        env.trim_blocks = True
+        env.lstrip_blocks = True
+        env.rstrip_blocks = True
+        env.filters['ipaddr'] = self.ipaddr
+        template = env.get_template(tmp_name_file)
+        output = template.render(unl_param=self.__lab_param, eve_ng_ip_host=self.__eveng_conn_param['ip'])
+        # env = Environment(loader=file_loader)
+        # env = Environment()
+        # env.filters['ipaddr'] = self.ipaddr
+        # with open(tmp_name_file) as file_:
+        #     self.template = Template(file_.read(), trim_blocks=True, lstrip_blocks=True)
+        # self.template.environment.filters["ipaddr"] = self.ipaddr
+        #output = template.render(unl_param=self.__lab_param, eve_ng_ip_host=self.__eveng_conn_param['ip'])
+        with open(out_file, mode='w') as file_:
+            file_.write(output)
+
+    def create_tbd_file(self, testbed_name_file):
+        self.__logger.info(f'Generate Cisco Testbed file: {testbed_name_file}')
+        for hst in self.__lab_param:
+            self.__logger.debug(f'{self.__lab_param[hst]}')
+        self.generate_jinja('testbed_eveng.j2', testbed_name_file)
+
+    def create_ansible_file(self):
+        self.__logger.info(f'Generate Ansible Hosts file: hosts.yml')
+        for hst in self.__lab_param:
+            self.__logger.debug(f'{self.__lab_param[hst]}')
+        self.generate_jinja('ansible_hosts.j2', 'hosts.yml')
+
+    @property
+    def unl_lab_param(self):
+        return self.__lab_param
+
+    @unl_lab_param.setter
+    def unl_lab_param(self, new_lab):
+        self.__lab_param = new_dv
+
+
+class TestbedConf:
+    """ Configure nodes using module pyaty/genie """
+
+    def __init__(self, file_testbed):
+        self.__testbed = testbed.load(file_testbed)
+        self.__init_exec_commands = []
+        self.__log_stdout = False
+        #self.__init_conf_commands = []
+        self.__lg = MyLogging(logging.INFO, "TestbedConf")
+        self.__logger = self.__lg.get_color_logger()
+        self.__logger.info(f'Initialized class TestbedConf')
+
+    def execute_testbed(self):
+        self.__logger.info(f'Configure devices ...')
+        for dev_name in self.__testbed.devices:
+            dev = self.__testbed.devices[dev_name]
+            self.__logger.info(f'Device: {dev.name}  OS: {dev.os}')
+            dev.connect(init_exec_commands=self.__init_exec_commands, log_stdout=self.__log_stdout)
+            assert dev.connected
+            for int_name in dev.interfaces:
+                dev_int = dev.interfaces[int_name]
+                self.__logger.info(f'Name_int: {int_name} ipv4: {dev_int.ipv4}')
+                dev_int.build_config(apply=True)
+            dev.configure(f'hostname {dev.name}')
+            dev.configure(self.__testbed.custom.postcommands['all'], timeout=60)
+            dev.configure(self.__testbed.custom.postcommands[dev.name], timeout=60)
+            dev.execute("wr mem")
+            # dev.disconnect()
+        # for conn in dev.connectionmgr.connections.values():
+        #     conn.disconnect()
+        dev.connectionmgr.connections.clear()
+
+    def run_testbed(self):
+        self.__logger.info(f'Run Testing of configuration')
+        file_path = "evenglibv2/evetestbed.py"
+        call(["python3", file_path])
+        return 0
---
+++ evenglibv2/evetestbed.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Configure S-Terra
+#
+# alexeykr@gmail.com
+# coding=utf-8
+# import codecs
+# Example
+# -------
+#
+#   short script designed to be run with a datafile
+#   (notice many expected values/parameters undefined)
+import logging
+import coloredlogs
+from pyats import aetest
+from genie import testbed as tbd
+from genie.conf import Genie
+import argparse
+from pyats.log.utils import banner
+import re
+logger = logging.getLogger(__name__)
+
+
+class common_setup(aetest.CommonSetup):
+    """ Common Setup section """
+    @aetest.subsection
+    def check_topology_links(self, testscript, testbed):
+        dev_links = dict()
+        for dev_first in testbed.devices.values():
+            dev_links[dev_first.name] = list()
+            for dev_sec in testbed.devices.values():
+                if dev_first != dev_sec:
+                    links = dev_first.find_links(dev_sec)
+                    dev_links[dev_first.name].append(links)
+        testscript.parameters['dev_links'] = dev_links
+
+    @aetest.subsection
+    def establish_connections(self, steps, testscript, testbed):
+        """ Common Setup subsection """
+        dev_connected = list()
+        for dev_name in testbed.devices.values():
+            with steps.start(f'Connect to  {dev_name.name}'):
+                dev_name.connect(init_exec_commands=[], log_stdout=False)
+            dev_connected.append(dev_name)
+        testscript.parameters['dev_conn'] = dev_connected
+
+
+class TestConfEveNG(aetest.Testcase):
+
+    @aetest.setup
+    def collects_ip_ping(self, testbed, testscript, dev_links, dev_conn):
+        dev_dest_ip = dict()
+        for name in dev_links:
+            dev_dest_ip[name] = list()
+            for rt in testbed.custom.postcommands[name]:
+                if re.search('ip\s*route', rt):
+                    routes = rt.split()[4]
+                    dev_dest_ip[name].append(routes)
+                    # print(rt.split()[4])
+            for links in dev_links[name]:
+                for lnk in links:
+                    for intf in lnk.interfaces:
+                        if str(intf.ipv4.ip) not in dev_dest_ip[name]:
+                            dev_dest_ip[name].append(str(intf.ipv4.ip))
+        testscript.parameters['dev_dest_ip'] = dev_dest_ip
+        aetest.loop.mark(self.ping, dev_router=dev_conn)
+
+    @aetest.test
+    def ping(self, steps, testbed, dev_dest_ip, dev_router, ):
+        for dst in dev_dest_ip[dev_router.name]:
+            with steps.start(f'Ping from {dev_router.name} to IP: {dst}'):
+                try:
+                    result = dev_router.ping(dst)
+                except Exception as e:
+                    self.failed('Ping {} from device {} failed with error: {}'.format(
+                        dst,
+                        dev_router.name,
+                        str(e)
+                    ))
+                else:
+                    match = re.search(r'Success rate is (?P<rate>\d+) percent', result)
+                    success_rate = match.group('rate')
+                    logger.info(banner('Ping {} with success rate of {}%'.format(
+                        dst,
+                        success_rate,
+                    )
+                    )
+                    )
+
+
+def eveng_testbed():
+    # testbed_file = "/Users/alex/Dropbox/MyProg/Python/evengv2/testbed_eveng.yaml"
+    testbed_file = "testbed_eveng.yaml"
+    aetest.main(testbed=Genie.init(testbed_file))
+
+
+if __name__ == '__main__':
+    eveng_testbed()
